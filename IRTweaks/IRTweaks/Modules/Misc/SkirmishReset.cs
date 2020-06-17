bc67f3d80a57608d94e5e376149fae226809baa5
Gerrit Kitts <gkitts@gmail.com>
2020-06-17 10:17:23 -0400

    Changing skirmishreset code to use harmony annos

--- IRTweaks/IRTweaks/Modules/Misc/SkirmishReset.cs
+++ IRTweaks/IRTweaks/Modules/Misc/SkirmishReset.cs
@@ -1,9 +1,17 @@
 ﻿using BattleTech;
 using BattleTech.Save;
+using Harmony;
 
 namespace IRTweaks.Modules.Misc {
+
+    // Resets all custom lances and units to allow a clean loading state
+    [HarmonyPatch(typeof(CloudUserSettings), "PostDeserialize")]
     public static class SkirmishReset {
-        public static void CloudUserSettings_PostDeserialize_Prefix(ref LastUsedLances ___lastUsedLances,
+
+        public static bool Prepare() => Mod.Config.Fixes.SkirmishReset;
+
+        public static void CloudUserSettings_PostDeserialize_Prefix(
+            ref LastUsedLances ___lastUsedLances,
             ref SkirmishUnitsAndLances ___customUnitsAndLances) {
             // Always reset the skirmish lances after serialization. This prevents the ever-spinny from missing mod pieces.
             ___lastUsedLances = new LastUsedLances();
