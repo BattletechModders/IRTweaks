f6363f2c71f52b80b360a38e95fa2e7ccdc67f17
Gerrit Kitts <gkitts@gmail.com>
2020-09-08 08:27:47 -0400

    - Correct MultiTargeting injection with new CustomUnits logic

--- IRTweaks/IRTweaks/Modules/Misc/PilotEffects.cs
+++ IRTweaks/IRTweaks/Modules/Misc/PilotEffects.cs
@@ -1,71 +1,68 @@
 ﻿using BattleTech;
 using Harmony;
-using System.Collections.Generic;
-using us.frostraptor.modUtils;
+using IRBTModUtils;
+using IRBTModUtils.Extension;
 
-namespace IRTweaks.Modules.Misc {
+namespace IRTweaks.Modules.Misc
+{
 
-    [HarmonyPatch(typeof(AbstractActor), "MaxTargets", MethodType.Getter)]
-    public static class AbstractActor_MaxTargets_Getter {
+    [HarmonyPatch(typeof(Pilot), "InitAbilities")]
+    public static class Pilot_InitAbilities
+    {
         static bool Prepare() { return Mod.Config.Fixes.MultiTargetStat; }
 
-        static void Postfix(AbstractActor __instance, ref int __result) {
-            Mod.Log.Trace?.Write($"AA:MT:G - entered.");
+        static void Postfix(Pilot __instance)
+        {
+            if (SharedState.Combat == null) return; // Nothing to do
 
-            if (__instance != null && __instance.StatCollection.ContainsStatistic(ModStats.EnableMultiTarget)) {
-                Mod.Log.Debug?.Write($"Multi-Target stat exists");
-                if (__instance.StatCollection.GetStatistic(ModStats.EnableMultiTarget).Value<bool>()) {
-                    Mod.Log.Debug?.Write($"Enabling multi-target for actor: {CombatantUtils.Label(__instance)}");
-                    __result = 3;
-                } else {
-                    Mod.Log.Debug?.Write($"Actor: {CombatantUtils.Label(__instance)} has enableMultiTarget: false");
-                }
+            if (__instance == null || __instance.ParentActor == null)
+            {
+                Mod.Log.Info?.Write("Pilot, ParentActor, or Combat is null! Cannot check for multi-targeting ability on unit, skipping!");
+                return;
             }
-        }
-    }
 
-    [HarmonyPatch(typeof(Pilot), "ActiveAbilities", MethodType.Getter)]
-    public static class Pilot_ActiveAbilities_Getter {
-        static bool Prepare() { return Mod.Config.Fixes.MultiTargetStat; }
+            bool enableMultiTarget = __instance.ParentActor.StatCollection.GetValue<bool>(ModStats.EnableMultiTarget);
+            Mod.Log.Debug?.Write($"Piloted unit {__instance.ParentActor.DistinctId()} has enableMultiTarget: {enableMultiTarget}");
 
-        static void Postfix(Pilot __instance, ref List<Ability> __result) {
-            Mod.Log.Trace?.Write($"AA:AA:G - entered.");
-
-            if (__instance == null || __instance.ParentActor == null) { return; }
-
-            if (__instance.ParentActor.StatCollection.GetStatistic(ModStats.EnableMultiTarget).Value<bool>()) {
-                Mod.Log.Debug?.Write($"Pilot: {__instance} needs multi-target ability");
-
+            if (enableMultiTarget)
+            {
                 bool hasMultiTarget = false;
-                foreach (Ability ability in __result) {
-                    if (ability.Def.Targeting == AbilityDef.TargetingType.MultiFire) {
+                foreach (Ability ability in __instance.Abilities)
+                {
+                    if (ability.Def.Targeting == AbilityDef.TargetingType.MultiFire)
+                    {
+                        Mod.Log.Debug?.Write(" -- unit already has MultiFire ability, skipping.");
                         hasMultiTarget = true;
+                        break;
                     }
                 }
 
-                if (!hasMultiTarget) {
-                    Traverse combatT = Traverse.Create(__instance).Property("Combat");
-                    CombatGameState combat = combatT.GetValue<CombatGameState>();
-
-                    if (combat == null) { return; }
-
-                    Mod.Log.Debug?.Write("  -- Adding multi-target ability to pilot.");
-                    AbilityDef abilityDef = combat.DataManager.AbilityDefs.Get(Mod.Config.Abilities.MultiTargetId);
+                if (!hasMultiTarget)
+                {
+                    Mod.Log.Debug?.Write("  -- adding multi-target ability to pilot.");
+                    AbilityDef abilityDef = SharedState.Combat.DataManager.AbilityDefs.Get(Mod.Config.Abilities.MultiTargetId);
                     Ability ability = new Ability(abilityDef);
-                    ability.Init(combat);
-                    __result.Add(ability);
+                    ability.Init(SharedState.Combat);
+                    __instance.Abilities.Add(ability);
+                    __instance.ActiveAbilities.Add(ability);
+                    __instance.StatCollection.ModifyStat("", -1, "MaxTargets", StatCollection.StatOperation.Set, 3);
+                    Mod.Log.Debug?.Write("  -- done.");
                 }
-                
             }
         }
     }
 
+
     [HarmonyPatch(typeof(AbstractActor), "InitEffectStats")]
-    public static class AbstractActor_InitEffectStats {
+    public static class AbstractActor_InitEffectStats
+    {
         static bool Prepare() { return Mod.Config.Fixes.MultiTargetStat; }
 
-        static void Postfix(AbstractActor __instance) {
+        static void Postfix(AbstractActor __instance)
+        {
             Mod.Log.Trace?.Write($"AA:MT:G - entered.");
+
+            Mod.Log.Debug?.Write($"Adding statistic: {ModStats.EnableMultiTarget} to unit: {__instance.DistinctId()}");
             __instance.StatCollection.AddStatistic<bool>(ModStats.EnableMultiTarget, false);
         }
     }
