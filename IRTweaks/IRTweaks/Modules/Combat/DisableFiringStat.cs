2b468f5e5fa3f3a3306f976f478b82ab11092b52
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2023-03-02 18:05:29 -0500

    weapon statistic to disable weapon from firing

---
+++ IRTweaks/IRTweaks/Modules/Combat/DisableFiringStat.cs
@@ -0,0 +1,44 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRTweaks.Modules.Combat
+{
+    public static class StatUtil
+    {
+        public static bool GetIsFiringDisabled(this Weapon weapon)
+        {
+            return weapon.StatCollection.GetValue<bool>("IsFiringDisabled");
+        }
+    }
+
+    [HarmonyPatch(typeof(Weapon), "InitStats",
+    new Type[] { })]
+    public static class Weapon_InitStats
+    {
+        public static void Postfix(Weapon __instance)
+        {
+            __instance.StatCollection.AddStatistic<bool>("IsFiringDisabled", false);
+        }
+    }
+
+    [HarmonyPatch(typeof(Weapon), "IsDisabled", MethodType.Getter)]
+    public static class Weapon_IsDisabled
+    {
+        //static bool Prepare() => !ModInit.modSettings.UsingMechAffinityForSwarmBreach;
+        public static void Postfix(Weapon __instance, ref bool __result)
+        {
+            if (!__result)
+            {
+                if (__instance.GetIsFiringDisabled())
+                {
+                    __result = true;
+                }
+            }
+        }
+    }
+}
